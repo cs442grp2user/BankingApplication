d61dc9f85eab85e4c521ba15a42447ed1e2c36f2
AbhishekK <akanch4@uic.edu>
2014-03-25 05:27:16 -0500

    Revised edited version of both account classes

--- src/cs442/group2/BankingApplication/Account.java
+++ src/cs442/group2/BankingApplication/Account.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Account implements Comparable<Account> {
@@ -29,14 +30,17 @@ public class Account implements Comparable<Account> {
 		this.accountType = accountType;
 		this.pin = pin;
 	}
-
+	
+	//there is a bug
 	public static Account getAccount(int accountID) {
-		Account account = new Account(1000, 100, "Checkings", 888);
+		Account account = null;
 		// Add DB code.
 		
 		Connection conn = BankConnect.getConnection();
 		if(conn!=null)	
 			System.out.println("Connection established successfully");
+		
+		String accID = "SELECT * FROM Account NATURAL JOIN AccountType WHERE accountID = ?;";
 			
 		try {
 			PreparedStatement statement = conn.prepareStatement(accID);
@@ -48,6 +52,28 @@ public class Account implements Comparable<Account> {
 			while(rs.next()){
 				int accType = rs.getInt("accounttypeid");
 				System.out.println("Value from result is "+accType);
+				
+				if(accType == 3) {
+					//Its a Credit account
+					CreditCard credit = null;
+					String selectcredit = "SELECT * FROM Credit NATURAL JOIN Account NATURAL JOIN AccountType WHERE accountID = ?;";
+					PreparedStatement stmtcredit = conn.prepareStatement(selectcredit);
+					stmtcredit.setInt(1, accountID);
+					ResultSet rscr = stmtcredit.executeQuery();
+					while(rscr.next()) {
+						credit = new CreditCard(rscr.getInt("accountid"), rscr.getInt("customerid"), rscr.getString("accounttypename"), 
+									rscr.getInt("pin"), rscr.getString("cardnumber"), rscr.getShort("cvv2"), rscr.getFloat("approvedcredit"));
+					}
+					return credit;
+				}
+				else {
+					//Its either a Checking, Savings or Rewards account
+					
+					Account acc = new Account(rs.getInt("accountid"), rs.getInt("customerid"), 
+									rs.getString("accounttypename"), rs.getInt("pin"));
+					
+					return acc;
+				}
 				
 			}
 			
@@ -87,14 +113,19 @@ public class Account implements Comparable<Account> {
 
 		return accounts;
 	}
-
+	
+	/**
+	 * Method returns balance for both Parent account class and child Credit class
+	 * @return
+	 */
 	public double getBalance() {
 		BigDecimal balance = null;
 		try {
 			Connection conn = BankConnect.getConnection();
+			
 			PreparedStatement statement = conn.prepareStatement(accountBalance);
 			statement.setInt(1, accountID);
-
+				
 			ResultSet rs = statement.executeQuery();
 			if (rs.next()) {
 				balance = rs.getBigDecimal("balance");
@@ -117,13 +148,14 @@ public class Account implements Comparable<Account> {
 		// Add DB Code
 		//System.out.println("Connection successful :"+conn);
 		
-		Connection conn = BankConnect.getConnection();
-		String sql = "SELECT * FROM customertransaction WHERE customerid = ? AND (fromaccountid = ? OR toaccountid = ?);";
+		String sql = "SELECT * FROM customertransaction WHERE fromaccountid = ? OR toaccountid = ?;";		//accommodates for this account involved in different customers transaction 
 		try {
+			Connection conn = BankConnect.getConnection();
+			
 			PreparedStatement statement = conn.prepareStatement(sql);
-			statement.setInt(1, this.getCustomerID());
+			//statement.setInt(1, this.getCustomerID());
+			statement.setInt(1, this.getAccountID());
 			statement.setInt(2, this.getAccountID());
-			statement.setInt(3, this.getAccountID());
 			ResultSet rs = statement.executeQuery();
 			
 			while(rs.next()){
@@ -143,13 +175,122 @@ public class Account implements Comparable<Account> {
 		}
 		return transactions;
 	}
-
+	
+	/**
+	 * This method will return a list of all Orders associated with the current account
+	 * @return
+	 */
 	public List<Order> orderHistory() {
 		ArrayList<Order> orders = new ArrayList<Order>();
+		HashMap<Integer, Order> orders_m = new HashMap<Integer, Order>();
 		// Add DB Code
+		
+		String sqlselect = "SELECT SO.orderid, SO.customerid, SO.timestamp, SO.orderpayment, OP.transactionid, OP.amountpaid from shoppingorder SO, orderpayment OP, customertransaction CT "+
+							"WHERE SO.orderid = OP.orderid AND OP.transactionid = CT.transactionid "+
+							"AND (CT.fromaccountid = ? OR CT.toaccountid = ?);";
+		
+		try {
+			Connection conn = BankConnect.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sqlselect);
+			statement.setInt(1, this.getAccountID());
+			statement.setInt(2, this.getAccountID());
+			ResultSet rs = statement.executeQuery();
+			
+			while(rs.next()){
+				int orderid = rs.getInt("orderid");
+				int transactionid = rs.getInt("transactionid");
+				double amtPaidinTransac = rs.getDouble("amountpaid");
+				
+				//Check whether the current order has been already accounted for
+				if(orders_m.containsKey(orderid)) {
+					OrderPayment op = new OrderPayment(orderid, 1, transactionid, amtPaidinTransac);
+					
+					orders_m.get(orderid).getPaymentOptions().add(op);
+					for(Order ord : orders) {
+						if(ord.getOrderID() == orderid) {
+							ord.getPaymentOptions().add(op);
+							break;
+						}
+						
+					}
+				}
+				//This is a new Order instance not yet put inside the ArrayList
+				else {
+					ArrayList<OrderPayment> paymentOptions = new ArrayList<OrderPayment>();
+					OrderPayment op = new OrderPayment(orderid, 1, transactionid, amtPaidinTransac);
+					paymentOptions.add(op);
+					
+					Order ord = new Order(orderid, rs.getInt("customerid"), rs.getDate("timestamp"), paymentOptions, rs.getDouble("orderpayment"));
+					
+					//Add this new Order to the HashMap and the ArrayList
+					orders_m.put(orderid, ord);
+					orders.add(ord);
+				}
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		return orders;
 	}
+	
+	/*public List<Order> orderHistory() {
+		ArrayList<Order> orders = new ArrayList<Order>();
+		HashMap<Integer, Order> orders_map = new HashMap<Integer, Order>();
+		
+		// Add DB Code
+		Connection conn = BankConnect.getConnection();
+		String select1 = "SELECT SO.orderid, SO.orderpayment, SO.timestamp,SO.customerid, OP.transactionid, OP.amountpaid "+ 
+						 "FROM shoppingorder SO, orderpayment OP "+
+						 "WHERE SO.orderid = OP.orderid AND SO.customerid = ?;";
+		
+		try {
+			PreparedStatement statement = conn.prepareStatement(select1);
+			statement.setInt(1, this.getCustomerID());
+			
+			ResultSet rs = statement.executeQuery();
+			while(rs.next()) {
+				int orderid = rs.getInt("orderid");
+				int custid = rs.getInt("customerid");
+				double amtPaidinTransac = rs.getDouble("amountpaid");
+				int transactionid = rs.getInt("transactionid");
+				
+				//Check whether the current order has been already accounted for
+				if(orders_map.containsKey(orderid)) {
+					OrderPayment op = new OrderPayment(orderid, 1, transactionid, amtPaidinTransac);
+					orders_map.get(orderid).getPaymentOptions().add(op);
+					
+					for(Order ord : orders) {
+						if(ord.getOrderID() == orderid) {
+							ord.getPaymentOptions().add(op);
+							break;
+						}
+						
+					}
+				}
+				//This is a new Order instance not yet put inside the ArrayList
+				else {
+					ArrayList<OrderPayment> paymentOptions = new ArrayList<OrderPayment>();
+					OrderPayment op = new OrderPayment(orderid, custid, transactionid, amtPaidinTransac);
+					paymentOptions.add(op);
+					
+					Order ord = new Order(orderid, custid, rs.getDate("timestamp"), paymentOptions, rs.getDouble("orderpayment"));
+					orders.add(ord);
+					orders_map.put(orderid, ord);
+				}
+				
+			}
+		} 
+		catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}
+		
+		return orders;
+	}*/
 
 	public int getAccountID() {
 		return accountID;
@@ -173,10 +314,6 @@ public class Account implements Comparable<Account> {
 	public int getPin() {
 		return pin;
 	}
-
-	private static String customerAccounts = "SELECT accountID, accountTypeName, pin FROM Customer NATURAL JOIN Account NATURAL JOIN AccountType WHERE customerID = ?; ";
-	private static String accountBalance = "SELECT balance FROM Account WHERE accountID = ?;";
-	private static String accID = "SELECT * FROM Account WHERE accountID = ?;";
 
 	/**
 	 * @return the customerID
@@ -248,9 +385,16 @@ public class Account implements Comparable<Account> {
 		this.pin = pin;
 	}
 	
+	private static String customerAccounts = "SELECT accountID, accountTypeName, pin FROM Customer NATURAL JOIN Account NATURAL JOIN AccountType WHERE customerID = ?; ";
+	private static String accountBalance = "SELECT balance FROM Account WHERE accountID = ?;";
+	
 	public static void main(String[] args)
 	{
-		Account.getAccount(1);	
+		Account account = Account.getAccount(19);	
+		System.out.println("Account ID is: "+account.getAccountID());
+		System.out.println("Account type is :"+account.getAccountType());
+		System.out.println("Account belongs to customer :"+account.getCustomerID());
+		System.out.println("Account PIN is: "+account.getPin());
 		
 		/*List<Account> accList = Account.getAllAccounts(1);
 		for(Account acc:accList){
@@ -260,8 +404,9 @@ public class Account implements Comparable<Account> {
 			
 		}*/
 		
-		//How is the Account type as String type here
-		Account acc = new Account(21, 3, "Checkings", 8888);
+		Account acc = new Account(14, 2, "Rewards", 8888);
+		//Account acc = new Account(21, 3, "Rewards",0);
+		
 		
 		/*List<Transaction> op_list = acc.accountHistory();
 		for(Transaction tr:op_list){
@@ -271,8 +416,14 @@ public class Account implements Comparable<Account> {
 			System.out.println("Transaction made to accountID: "+tr.getToAccountID());
 			
 			System.out.println("------------Next Transaction--------------");
-		}*/
+		}
+		*/
 		
-		
+		List<Order> resultList = acc.orderHistory();
+		for(Order order : resultList) {
+			
+			System.out.println("Order involved with Account:"+acc.getAccountID() + " is:"+order.getOrderID() );
+		}
+				
 	}
 }
--- src/cs442/group2/BankingApplication/CreditCard.java
+++ src/cs442/group2/BankingApplication/CreditCard.java
@@ -1,13 +1,5 @@
 package cs442.group2.BankingApplication;
 
-import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class CreditCard extends Account{
 	
 	private String cardNumber;
@@ -16,7 +8,7 @@ public class CreditCard extends Account{
 	private float approvedCredit;
 	
 		
-	private CreditCard(int accountID, int customerID, String accountType, int pin, String cardNumber,short cvv2,float approvedCredit) {
+	protected CreditCard(int accountID, int customerID, String accountType, int pin, String cardNumber,short cvv2,float approvedCredit) {
 		
 		super(accountID, customerID, accountType, pin);
 		this.cardNumber = cardNumber;
@@ -24,43 +16,53 @@ public class CreditCard extends Account{
 		//this.availableBalance = availableBalance;
 		this.approvedCredit = approvedCredit;		
 		// TODO Auto-generated constructor stub
-		}
-	
-	
-	public static CreditCard getAccount(int accountID) {
-		CreditCard creditAccount = new CreditCard(1000, 100, "Credit", 888, "8888888888888882",(short)888,1000);
-		// Add DB code.
-		
-		Connection conn = BankConnect.getConnection();
-		if(conn!=null)	
-			System.out.println("Connection established successfully");
-			
-		try {
-			PreparedStatement statement = conn.prepareStatement(accID);
-			statement.setInt(1, accountID);
-			ResultSet rs = statement.executeQuery();
-			
-				
-			while(rs.next()){
-				String cardno = rs.getString("cardnumber");
-				System.out.println("Value from the result - Credit card number "+ cardno);
-				
-			}
-			
-		} catch (SQLException e) {
-		// TODO Auto-generated catch block
-			e.printStackTrace();
-			Reporting.err.println(e);
-		}
- 		
-		return creditAccount; // its OK to return null as account may not exist
 	}
-	private static String accID = "SELECT * FROM Credit WHERE accountID = ?;";
 	
-public static void main(String[] args){
-		
-		CreditCard cc = new CreditCard(1000, 100, "Credit", 888, "8888888888888882",(short)888,1000);
-		cc.getAccount(14);
-		//System.out.println() ;
-	}	
+	/**
+	 * @return the cardNumber
+	 */
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+
+	/**
+	 * @param cardNumber the cardNumber to set
+	 */
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+
+	/**
+	 * @return the cvv2
+	 */
+	public short getCvv2() {
+		return cvv2;
+	}
+
+
+	/**
+	 * @param cvv2 the cvv2 to set
+	 */
+	public void setCvv2(short cvv2) {
+		this.cvv2 = cvv2;
+	}
+
+
+	/**
+	 * @return the approvedCredit
+	 */
+	public float getApprovedCredit() {
+		return approvedCredit;
+	}
+
+
+	/**
+	 * @param approvedCredit the approvedCredit to set
+	 */
+	public void setApprovedCredit(float approvedCredit) {
+		this.approvedCredit = approvedCredit;
+	}
+	
 }
