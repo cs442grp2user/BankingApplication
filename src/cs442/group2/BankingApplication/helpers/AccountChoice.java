a3251d4cb24e16aa6aaa62733cdd5fe92cb738ef
AbhishekK <akanch4@uic.edu>
2014-04-07 21:23:51 -0500

    latest updated code before working on UI connectivity

--- src/cs442/group2/BankingApplication/helpers/AccountChoice.java
+++ src/cs442/group2/BankingApplication/helpers/AccountChoice.java
@@ -5,36 +5,6 @@ import cs442.group2.BankingApplication.Account;
 public class AccountChoice {
 	Account account;
 	double deductAmount;
-	
-	/**
-	 * @return the account
-	 */
-	public Account getAccount() {
-		return account;
-	}
-
-	/**
-	 * @param account the account to set
-	 */
-	public void setAccount(Account account) {
-		this.account = account;
-	}
-
-	/**
-	 * @return the deductAmount
-	 */
-	public double getDeductAmount() {
-		return deductAmount;
-	}
-
-	/**
-	 * @param deductAmount the deductAmount to set
-	 */
-	public void setDeductAmount(double deductAmount) {
-		this.deductAmount = deductAmount;
-	}
-
-	
 
 	@Override
 	public String toString() {
